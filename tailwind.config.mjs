b2156db0d2f5aec089a2b9b6a175ea93aa9b1232
karlo <karlsgit@gmail.com>
2024-07-20 12:40:13 +0800

    added responsive navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            screens: {
+                sm: "576px",
+                md: "768px",
+                lg: "992px",
+                xl: "1200px",
+                "2xl": "1400px",
+            },
+
+            colors: {
+                primary: "#6c63ff",
+                smoke: "#797979",
+                dim: "#f3f9fb",
+            },
+        },
+    },
+    plugins: [],
+};
