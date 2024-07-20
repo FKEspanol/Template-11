2b32f24d2fa49f7cbda61e19727ce644273c9a1c
karlo <karlsgit@gmail.com>
2024-07-20 08:36:52 +0800

    basic layout and added third party libraries

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+const SITE_TITLE = "Template 11";
+const SITE_DESCRIPTION =
+    "web development, frontend development, frontend framewerk, astro js, html, css, javascript, modern web development";
+
+export { SITE_TITLE, SITE_DESCRIPTION };
